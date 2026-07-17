94ba89d4630bcf0e6d49d6e828286d5f46b8fc55
Aaditya Chaudhary <aadityachaudhary229@gmail.com>
2026-07-17 19:57:06 +0545

    Fix: reduce main top padding on mobile to eliminate header/hero gap

--- app/layout.tsx
+++ app/layout.tsx
@@ -36,7 +36,7 @@ export default function RootLayout({
       >
         <LanguageProvider>
           <Header />
-          <main className="min-h-screen pt-[112px] md:pt-[128px]">
+          <main className="min-h-screen pt-[80px] md:pt-[128px]">
             {children}
           </main>
           <Footer />
